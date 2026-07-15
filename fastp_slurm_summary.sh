2f1c36a9d4d20da7a05ca33d1843c82aa2cde481
huiqingyeooo <yeohuiqing92@gmail.com>
2026-07-15 16:59:53 -0600

    update scripts

---
+++ fastp_slurm_summary.sh
@@ -0,0 +1,10 @@
+fastpOutput=/work/soghigian_lab/huiqing.yeo/pipiens/0_trimmedReads/lane1
+
+# check slurm log files
+for i in $(ls ${fastpOutput}/slurm-*.out);
+do 
+col1=`echo ${i}`
+col2=`tail -n 1 ${i}`
+echo $col1 $col2 >> ${fastpOutput}/slurm_summary.txt
+done
+
